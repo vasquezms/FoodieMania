7bdaba6f54d0120eb5254e26319796476eefda4e
SANTIAGO MU�OZ VASQUEZ <santiagomunoz298536@correo.itm.edu.co>
2024-06-03 16:29:20 -0500

    create initialDB

---
+++ FoodieMania/DAL/DataBaseContext.cs
@@ -0,0 +1,22 @@
+﻿
+using FoodieMania.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace FoodieMania.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Foodie>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
+
---
+++ FoodieMania/DAL/Entities/Foodie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebAPI.DAL.Entities;
+
+namespace FoodieMania.DAL.Entities
+{
+    public class Foodie : AuditBase
+    {
+        [Display(Name = "Comida rapida")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "Es campo {0} es obligatorio")]
+        public string Name { get; set; }
+
+    }
+}
